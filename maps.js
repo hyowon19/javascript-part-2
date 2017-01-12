3010c4ebc3286eb55c6e6fcaf720256edd51c94f
Victor Wong <vj1901@hotmail.com>
2017-01-12 15:22:26 +0000

    maps.js

--- maps.js
+++ maps.js
@@ -8,18 +8,49 @@ function Tile (type) {
             return false;
         }
     }
+    this.randomTile = function() {
+        var tileNum = Math.floor(Math.random() * 3);
+        if (tileNum === "1") {
+            this.type = "grass";
+        }
+        else if (tileNum === "2") {
+            this.type = "sand";
+        }
+        else {
+            this.type = "water";
+        }
+    }
 }
 
 function Map (width, height) {
     this.width = width;
     this.height = height;
-    this.tiles = function(width, height) {
-        tMap = [];
-        for (var i = 0; i < width < i++) {
-            tmap[i] = [];
-            for (var j = 0; j < height < j++) {
-                tmap[j] = 
+    this.tiles = function() {
+        var tMap = [];
+        for (var i = 0; i < width ; i++) {
+            tMap.push([]);
+            for (var j = 0; j < height ; j++) {
+                tMap[i].push(1);
             }
         }
+        return tMap;
     }
-}
+};
+
+var houston = new Map(10,10);
+console.log(houston);
+console.log(houston.tiles());
+// var f = [];
+// for(var i = 0; i < 2; i++) {
+//   f.push([]);
+//   for( var j = 0; j < 2; j++) {
+//     f[i].push(1);
+//   } 
+// }
+
+// for (var i = 0; i < 2; i++) {
+//   f[i] = []
+//   for (var j = 0; j < 2; j++) {
+//     f[i][j]=1;
+//   }
+// }
